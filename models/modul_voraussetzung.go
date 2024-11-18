ae77088889b00e5b038d0a0433253af5f5778c40
Max <Maximilian_Cosmo.Barth@stud.htwk-leipzig.com>
2024-11-18 02:25:17 +0100

    added studiengang id to modul_vorraussetzung struct

--- models/modul_voraussetzung.go
+++ models/modul_voraussetzung.go
@@ -1,6 +1,7 @@
 package models
 
 type ModulVoraussetzung struct {
+	StudiengangID               int    `json:"studiengang_id"`
 	ModulKuerzel                string `json:"modul_kuerzel"`
 	ModulVersion                int    `json:"modul_version"`
 	VorausgesetztesModulKuerzel string `json:"vorausgesetztes_modul_kuerzel"`
